c743a3118e634e86f7e12830097bd648fd386a0b
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2015-03-02 17:49:57 +0100

    Mean errors per text color / semantic color

--- analytics/processors/ExperimentSTROOPProcessor.py
+++ analytics/processors/ExperimentSTROOPProcessor.py
@@ -31,6 +31,34 @@ class ExperimentSTROOPProcessor(ExperimentProcessor):
 
         return results, profile
 
+    # Big mess. Don't try to go through it. Better see ExperimentProcessor.get_data
+    def get_data2d(self, results, var, nvars, dep1, dep2, levels1, levels2, scaling = 1):
+        resd = {}
+        datas = []
+        for interface in levels1:
+            exps = self.gen_reg_exp(nvars-1, dep2-1, levels2)
+            data = []
+            for key, val in exps.iteritems():
+                valint = val + "/" + interface
+                res = results.filter(key__iregex=r'^' + valint + '/' + var + '$')
+                v = self.mean_values(res) * scaling
+                data.append([key, v])
+            datas.append(data)
+
+        for v in datas[0]:
+            resd[v[0]] = []
+
+        for data in datas:
+            for v in data:
+                resd[v[0]].append(v[1])
+
+        print datas
+        print resd
+        res = []
+        for dt in datas[0]:
+            res.append([dt[0]] + resd[dt[0]])
+        return res
+
     def process_results(self, results):
         # Format:
         #  - experiment in a row 1-5
@@ -50,5 +78,12 @@ class ExperimentSTROOPProcessor(ExperimentProcessor):
         processed.append({'data': self.get_data(results, "errors", 5, 3, ["red","blue","green","yellow","brown"]), 'title': "Mean errors per text color", 'var': "Mean errors", 'type': 'barplot'})
         processed.append({'data': self.get_data(results, "errors", 5, 4, ["red","blue","green","yellow","brown"]), 'title': "Mean errors per text semantics", 'var': "Mean errors", 'type': 'barplot'})
  
+        colors = ["blue", "brown", "green", "yellow", "red"]
+        header = [[""] + colors]
+        data = self.get_data2d(results, "errors", 5, 3, 4, colors, colors, scaling = 1)
+        data.sort()
+        processed.append({'data': header + data, 'title': "Mean errors per text color / semantic color", 'var': "Mean errors", 'type': 'table'})
+
+
         return processed
 
